685b002076fcf4ac71fd0f8cf19eeb2e6d8eebc9
PULKIT VAID <72128672+gitpulkit@users.noreply.github.com>
2020-10-20 13:48:47 +0530

    Add files via upload

---
+++ Update.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Update.aspx.cs" Inherits="Update" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+       <style >
+        .block{
+    height: 35px;
+    border-radius: 11px;
+        width: 100%;
+
+        }
+        h2{
+            text-align: center;
+    font-size: 41px;
+    font-family: calgiri;
+    font-weight: bold;
+        }
+        .btn{
+          
+    background-color: cadetblue;
+    height: 41px;
+    font-size: 20px;
+    font-weight: bold;
+    text-align:center;
+     margin-top: 11PX;
+        }
+        .aa h4{
+          font-weight: bold;
+    padding-top: 31px;  
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div class="container">
+      <h2>Registration Form</h2>
+       <div class="row">
+           <div class="col-xs-6"><div class="aa"><h4>First Name</h4><input class="block" type="text" id="TextBox1" runat="server" /></div></div>
+                <div class="col-xs-6"><div class="aa"><h4>Last Name</h4><input class="block" type="text" id="TextBox2" runat="server" /></div></div>
+               </div>
+         <div class="row">
+           <div class="col-xs-6"><div class="aa"><h4>Contact No.</h4><input class="block" type="text" id="TextBox3" runat="server"/></div></div>
+                <div class="col-xs-6"><div class="aa"><h4>E-Mail</h4><input class="block" type="text" id="TextBox4" runat="server" /></div></div>
+               </div>
+       
+    <div class="row">
+           <div class="col-xs-6"><div class="aa"><h4>Password</h4><input class="block" type="text" id="TextBox5" runat="server" /></div></div>
+                
+               </div><br />
+     
+   </div>
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Show" />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Update" />
+    <br />
+    
+    </div>
+    
+    
+    
+</asp:Content>
+
---
+++ Update.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+public partial class Update : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string connectionString = null;
+        SqlConnection cnn;
+        SqlCommand command;
+        connectionString = "Data Source=VEDANSH-PC; Integrated Security=true;Initial Catalog= Registration; uid=YourUserName; Password=yourpassword";
+        cnn = new SqlConnection(connectionString);
+        cnn.Open();
+        string sql = "update reg set FirstName='"+ TextBox1.Value+ "' where LastName='" + TextBox2.Value + "'";
+        command = new SqlCommand(sql, cnn);
+
+        command.ExecuteNonQuery();
+        Response.Write("<script>alert('Update');</script>");
+        command.Dispose();
+        cnn.Close();
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string connectionString = null;
+        SqlConnection cnn;
+        SqlCommand command;
+        connectionString = "Data Source=VEDANSH-PC; Integrated Security=true;Initial Catalog= Registration; uid=YourUserName; Password=yourpassword";
+        cnn = new SqlConnection(connectionString);
+        cnn.Open();
+        SqlDataReader read = (null);
+
+        string sql = "select * from reg where FirstName='" + TextBox1.Value + "'";
+        command = new SqlCommand(sql, cnn);
+        command.ExecuteNonQuery();
+        read = command.ExecuteReader();
+        read.Read();
+        TextBox1.Value = (read["FirstName"].ToString());
+        TextBox2.Value = (read["LastName"].ToString());
+        TextBox3.Value = (read["ContactNo"].ToString());
+        TextBox4.Value = (read["Password"].ToString());
+        TextBox5.Value = (read["EMail"].ToString());
+
+        
+        command.Dispose();
+        cnn.Close();
+    }
+}
